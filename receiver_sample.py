476b335f9d96d8c8ef489e05264d3834ef6d799b
Ahmet <33777806+ahmetozdemir73@users.noreply.github.com>
2023-02-25 14:13:13 +0300

    Update and rename receiver.py to receiver_sample.py

---
+++ receiver_sample.py
@@ -0,0 +1,17 @@
+from UDP_transfer import UDPServer
+import json
+import time
+
+
+with open('conf.json', 'r') as file:
+    f = json.load(file)
+host = f['receiver_ip']
+
+
+udp = UDPServer(host=host, port=5000)
+
+
+# Continuously receive data
+while True:
+    data = udp.receive_data()
+    print(data)
